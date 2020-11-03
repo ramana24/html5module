b85da7197fad28cfd4416bd6bcaf5d9d4ef95335
ramana24 <yellapuramana@gmail.com>
2020-11-03 02:37:34 +0000

    intial commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 sap.ui.define([
 		"sap/ui/core/mvc/Controller"
 	],
@@ -10,6 +11,32 @@ sap.ui.define([
 		return Controller.extend("sap.cp.HTML5Module.controller.View1", {
 			onInit: function () {
 
-			}
+                var oModel = new sap.ui.model.json.JSONModel();
+                // option 1 binding data
+                oModel.setData({
+    firstName: "Ramana",
+    lastName: "Y"
+});
+
+ 
+
+// @ts-ignore
+this.getView().byId("idMain").setModel(oModel);
+
+
+
+
+
+      // console.log("onInit executed");
+            },
+            onAfterRendering:function(){
+        //        console.log("onAfterRendering executed");
+            },
+            onBeforeRendering:function(){
+          //      console.log("onBefore Rendering executed");
+            },
+            onExit:function(){
+            //    console.log("onExit executed");
+            }
 		});
 	});
