602d99d7f47273837ddb9e4c91aee2dfd3385f51
ramana24 <yellapuramana@gmail.com>
2020-11-04 03:56:48 +0000

    commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -11,15 +11,16 @@ sap.ui.define([
 		return Controller.extend("sap.cp.HTML5Module.controller.View1", {
 			onInit: function () {
 
-                var oModel = new sap.ui.model.json.JSONModel();
-                // option 1 binding data
+                var oModel = new sap.ui.model.json.JSONModel(); // new model - url
+                // option 1  loading data into model from either mock json file or database odata 
                 oModel.setData({
-    firstName: "Ramana",
-    lastName: "Y"
-});
+                firstName: "Ramana",
+                lastName: "Y"
+                }  );
 
- 
+   // var oModelData = new sap.ui.model.odata.oDataModel("https://sapes5.sapdevcenter.com/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/$metadata");
 
+   // Now we assign the model to View
 // @ts-ignore
 this.getView().byId("idMain").setModel(oModel);
 
@@ -27,16 +28,16 @@ this.getView().byId("idMain").setModel(oModel);
 
 
 
-      // console.log("onInit executed");
+       console.log("onInit executed");
             },
             onAfterRendering:function(){
-        //        console.log("onAfterRendering executed");
+                console.log("onAfterRendering executed");
             },
             onBeforeRendering:function(){
-          //      console.log("onBefore Rendering executed");
+                console.log("onBefore Rendering executed");
             },
             onExit:function(){
-            //    console.log("onExit executed");
+               console.log("onExit executed");
             }
 		});
 	});
